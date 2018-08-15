6e90fff6366af56a6d782a28748e04ca7f0955c8
Poo <alfredolvs@gmail.com>
2018-08-15 08:18:23 -0300

    Iniciando repositorio

--- src/main/java/poo/Principal.java
+++ src/main/java/poo/Principal.java
@@ -4,5 +4,6 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("ola mundo");
         System.out.println("tchau mundo");
+        System.out.println("de novo mundo");
     }
 }
